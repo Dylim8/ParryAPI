d8fec21de63f4b90de68b65cd68fd9b54dfe3689
Dylim8 <50117181+Dylim8@users.noreply.github.com>
2024-02-08 13:06:12 -0800

    restructure and training

--- parry_sentiment_script.py
+++ parry_sentiment_script.py
@@ -12,6 +12,7 @@ additional_terms = {
     'wp': 1.1,
     'ns': 1.0,
     'ggwp': 1.0,
+    'glhf': 1.0,
     'kys': -2.0
 }
 
@@ -19,10 +20,23 @@ def vader_update():
     analyzer.lexicon.update(additional_terms)
     print('updated')
 
+def testing():
+    text: str = input('Text: ')
+    vs = analyzer.polarity_scores(text)
+    print('{:-<65} {}'.format(text, str(vs)))
+
+def training():
+    text: str = input('Text: ')
+    words = text.split()
+    for word in words:
+        if word.lower() in analyzer.lexicon:  # Convert to lowercase to match the lexicon's format
+            print(f"'{word}' is in the lexicon with a sentiment score of {analyzer.lexicon[word.lower()]}")
+        else:
+            print(f"'{word}' is not in the lexicon.")
 
 if __name__ == '__main__':
     vader_update()
     while True:
-        text: str = input('Text: ')
-        vs = analyzer.polarity_scores(text)
-        print('{:-<65} {}'.format(text, str(vs)))
+        #testing()
+        training()
+    
